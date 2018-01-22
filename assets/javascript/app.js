0f1df1465d452b5083b3380b69f3554599d2d303
Rodesigned <rodesigned@att.net>
2018-01-22 00:45:26 -0500

    updated files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,137 +5,159 @@
 // 2. Create button for adding trains - then update the html + update the database
 // 3. Create a way to retrieve users from the firebase database.
 // 4. Use Moment js to calculate the difference between train times
-// 5. Bonus use the firebase database to update and remove trains
-
+// 5. Bonus use the firebase database to update and remove trains 
 
 // Initialize Firebase
-  var config = {
+var config = {
     apiKey: "AIzaSyCVA5OJpsiDVOMsjZG8A9jaz8HwZsXwDGA",
     authDomain: "bootcamptrain-1.firebaseapp.com",
     databaseURL: "https://bootcamptrain-1.firebaseio.com",
     projectId: "bootcamptrain-1",
     storageBucket: "bootcamptrain-1.appspot.com",
     messagingSenderId: "697351375869"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-// Create a variable to reference the database.
-var database = firebase.database();	
+//Run Clock  
+setInterval(function() {
+    $('.current-time').html(moment().format('hh:mm:ss A'));
+}, 1000);
+
+
 // Initial Values
-var trainName = "";
-var trainDestination = "";
-var trainTime = "";
-var trainFrequency = "";
+var database = firebase.database();
+var fbTime = moment();
+var newTime;
 // Added variable to manipulate key to update the table content
 var keyToUpdate = '';
-	
-// Create button event to clear trains
-function clearTrains(event) {
-    event.preventDefault();
-    $("#trainName").val('');
-    $("#trainDest").val('');
-    $("#trainTime").val('');
-    $("#trainFreq").val('');
-}
-	
-	// Create button event for user to add trains
-$("#add_trains").on("click", function (event) {
-    event.preventDefault();
 
+// Create button event for user to add trains
+$(".submit").on("click", function(event) {
+    event.preventDefault();
     // Use the variables and form id's for user input
-    trainName = $("#trainName").val().trim();
-    trainDestination = $("#trainDest").val().trim();
-    trainTime = $("#trainTime").val().trim();
-    trainFrequency = $("#trainFreq").val().trim();
+    var trainName = $('#trainName').val().trim();
+    var trainDestination = $('#trainDestination').val().trim();
+    var trainTime = moment($('#firstTrain').val().trim(), "HH:mm").format("X");
+    var trainFreq = $('#trainFrequency').val().trim();
 
     // Creates local "temporary" object for holding the trains data
-	// And use if statement to push data to database only if input fileds are not empty
-    if (trainName !== '' && trainDestination !== '' && trainTime !== '' && trainFrequency !== '') {
-    var trains = {
-        trainName: trainName,
-        trainDestination: trainDestination,
-        trainTime: trainTime,
-        trainFrequency: trainFrequency
-    };
-	// Uploads data to firebase database
+    // And use if statement to push data to database only if input fileds are not empty
+    if (trainName !== '' && trainDestination !== '' && trainTime !== '' && trainFreq !== '') {
+        // Clears all of the text-boxes
+        $('#trainName').val('');
+        $('#trainDestination').val('');
+        $('#firstTrain').val('');
+        $('#trainFrequency').val('');
+        $('#trainKey').val('');
 
+        fbTime = moment().format('X');
+        // Push to firebase
         if (keyToUpdate === '') {
-            database.ref().push(trains);
-
-        } else {
-            database.ref(keyToUpdate).update(trains);
+            database.ref().child('trains').push({
+                trainName: trainName,
+                trainDestination: trainDestination,
+                trainTime: trainTime,
+                trainFreq: trainFreq,
+                currentTime: fbTime,
+            });
+        } else if (keyToUpdate !== '') {
+            database.ref('trains/' + keyToUpdate).update({
+                trainName: trainName,
+                trainDestination: trainDestination,
+                trainTime: trainTime,
+                trainFreq: trainFreq,
+                currentTime: fbTime,
+            });
+            keyToUpdate = '';
         }
-        keyToUpdate = '';
 
     } else {
         return;
     }
 
-   // Clears all of the text-boxes
-    $("#trainName").val("");
-    $("#trainDest").val("");
-    $("#trainTime").val("");
-    $("#trainFreq").val("");
-});
-	
-	// Create Firebase event for adding trains data to the database and a row in the HTML when a user adds an entry
-    database.ref().on("child_added", function (snapshot) {
-	// store the variables value in the database
-    var content = snapshot.val();
-    // Create the variables to hold the train values
-    var trainName = content.trainName;
-    var trainDestination = content.trainDestination;
-    var trainTime = content.trainTime;
-    var trainFrequency = content.trainFrequency;
-     // Use key to update and remove data
-	var trainKey = snapshot.key;
-
-    // Create the convert time for the trains
-    var timeConversion = moment(trainTime, "hh:mm");
-    // Moment js calculates the difference between train times
-    var diffTime = moment().diff(moment(timeConversion), "minutes");
-   // Moment js calculates the train times away %=remainder
-    var timeRemainder = diffTime % trainFrequency;
-   // Moment js calculates the minutes until next train
-    var minutesAway = trainFrequency - timeRemainder;
-    // Moment js calculates the arrival of the next train
-    var nextTrain = moment().add(minutesAway, "minutes").format("hh:mm");
-    
-   // Each train's data appended into the table
-   $("#train_table>tbody").append(
-	   
-"<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainFrequency + "</td><td>" + nextTrain + "</td><td>" + minutesAway + "</td><td>" + "<button class='updateTrainBtn' data-train=" + trainKey + ">Update</button>" + "</td><td>" + "<button class='removeTrainBtn' data-train=" + trainKey + ">Remove</button>" + "</td></tr>"
-);	
-		
-
-
 });
 
-// Use firebase key to store data then remove
-// Create variable to store data attribute 
-function removeTrain() {
-    var keyToRemove = $(this).attr("data-train");
-    database.ref(keyToRemove).remove();
-    $(this).closest('tr').remove();
+// Update minutes away 
+function timeUpdater() {
+    database.ref().child('trains').once('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            fbTime = moment().format('X');
+            database.ref('trains/' + childSnapshot.key).update({
+                currentTime: fbTime,
+            });
+        });
+    });
 }
 
-//Use firebase key to store data then update
-function updateTrain() { 
-    keyToUpdate = $(this).attr("data-train");
-    database.ref(keyToUpdate).once('value').then(function (childSnapshot) {
+setInterval(timeUpdater, 10000);
 
-        $("#trainName").val(childSnapshot.val().trainName);
-        $("#trainDest").val(childSnapshot.val().trainDestination);
-        $("#trainTime").val(childSnapshot.val().trainTime);
-        $("#trainFreq").val(childSnapshot.val().trainFrequency);
-      });
-}
-	
-// Calls clearForm function when user click on Clear button to remove form input data in the html page
-$(document).on("click", ".clearButton", clearTrains);
-// Calls updateTrain function when user click on Update button to update train info
-$(document).on("click", ".updateTrainBtn", updateTrain);
-// Calls removeTrain function when user click on Delete button to remove train
-$(document).on("click", ".removeTrainBtn", removeTrain);
-	
-	
+
+// Database reference to firebase
+database.ref().child('trains').on('value', function(snapshot) {
+    $('tbody').empty();
+
+    snapshot.forEach(function(childSnapshot) {
+        var trainClass = childSnapshot.key;
+        var trainId = childSnapshot.val();
+        var firstTimeConverted = moment.unix(trainId.trainTime);
+        var timeDiff = moment().diff(moment(firstTimeConverted, 'HH:mm'), 'minutes');
+        var timeDiffCalc = timeDiff % parseInt(trainId.trainFreq);
+        var timeDiffTotal = parseInt(trainId.trainFreq) - timeDiffCalc;
+
+        if (timeDiff >= 0) {
+            newTime = null;
+            newTime = moment().add(timeDiffTotal, 'minutes').format('hh:mm A');
+
+        } else {
+            newTime = null;
+            newTime = firstTimeConverted.format('hh:mm A');
+            timeDiffTotal = Math.abs(timeDiff - 1);
+        }
+        // Each train's data appended into the table
+        $('tbody').append("<tr class=" + trainClass + "><td>" + trainId.trainName + "</td><td>" +
+            trainId.trainDestination + "</td><td>" +
+            trainId.trainFreq + "</td><td>" +
+            newTime + "</td><td>" +
+            timeDiffTotal + "</td><td>" + "<button class='removeTrainBtn' data-train=" + trainClass + ">Remove</button>" + "</td></tr>"
+        );
+
+    });
+}, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
+
+// Database reference to firebase
+database.ref().child('trains').on('child_changed', function(childSnapshot) {
+
+    var trainClass = childSnapshot.key;
+    var trainId = childSnapshot.val();
+    var firstTimeConverted = moment.unix(trainId.trainTime);
+    var timeDiff = moment().diff(moment(firstTimeConverted, 'HH:mm'), 'minutes');
+    var timeDiffCalc = timeDiff % parseInt(trainId.trainFreq);
+    var timeDiffTotal = parseInt(trainId.trainFreq) - timeDiffCalc;
+
+    if (timeDiff > 0) {
+        newTime = moment().add(timeDiffTotal, 'minutes').format('hh:mm A');
+    } else {
+        newTime = firstTimeConverted.format('hh:mm A');
+        timeDiffTotal = Math.abs(timeDiff - 1);
+    }
+
+    $('.' + trainClass).html("<td>" + trainId.trainName + "</td><td>" +
+        trainId.trainDestination + "</td><td>" +
+        trainId.trainFreq + "</td><td>" +
+        newTime + "</td><td>" +
+        timeDiffTotal + "</td><td>" + "<button class='removeTrainBtn' data-train=" + trainClass + ">Remove</button>" + "</td></tr>"
+    );
+
+}, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
+
+// Create button to remove train attribute
+$(document).on('click', '.removeTrainBtn', function() {
+    var trainKey = $(this).attr('data-train');
+    database.ref("trains/" + trainKey).remove();
+    $('.' + trainKey).remove();
+});
+
